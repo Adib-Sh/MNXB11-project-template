f5a71cae411341701b82ca947b157d00a7245d14
Adib Shaker <neoadib@gmail.com>
2024-11-09 08:02:20 +0100

    fixed Lyra help and input

--- main.cxx
+++ main.cxx
@@ -14,34 +14,35 @@ using namespace std::chrono;
 
 int main(int argc, char *argv[]) {
     std::string filename;
-    bool verbose = false;
+    bool showHelp = false;
 
-    // Define the command-line interface using Lyra
+    // Create a command line parser using Lyra
     auto cli = lyra::cli()
-        .add_argument(lyra::arg(filename, "filename").required()
-            .help("Path to the CSV file"))
-        .add_argument(lyra::arg(verbose, "verbose").optional()
-            .help("Enable verbose output"));
+        | lyra::help(showHelp).description("This program will help you upload and proccess the CSV files.") // Help command -h
+        | lyra::opt(filename, "input_file")
+            ["-i"]["--input-file"]("Name of the input CSV file");
 
-    // Parse the arguments
-    auto result = cli.parse({argc, argv});
-    if (!result) {
-        std::cerr << "Error: " << result.message() << std::endl;
-        return 1;
-    }
+    auto responce = cli.parse({argc, argv});
 
+    // Check responce
+    if (!responce) {
+            std::cerr << "Error: " << responce.message() << std::endl;
+            return 1;
+        }
+    
+    if (showHelp) {
+        std::cout << cli << std::endl;
+        return 0; 
+        }
+
+    // Check if the filename is provided
     if (filename.empty()) {
-        std::cerr << "No filename provided!" << std::endl;
+        std::cerr << "Error: No input file provided. Use -i or --input-file to specify the file." << std::endl;
         return 1;
     }
 
-
-    // Check if a filename is provided
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <csv_file>" << std::endl;
-        return 1; // Return error code
-    }
-
+    // Confirms the filename
+    std::cout << "Input file: " << filename << std::endl;
 
     // Create a CSV reader object
     io::CSVReader<4> reader(filename); // Assuming there are 5 columns, but we'll skip the 4th
@@ -50,7 +51,6 @@ int main(int argc, char *argv[]) {
     // Define variables for each column
     int day, year, month;
     double measurement;
-
 
     // Read each row and print date and measurement with the corresponding weekday
     while (reader.read_row(day, year, month, measurement)) {
